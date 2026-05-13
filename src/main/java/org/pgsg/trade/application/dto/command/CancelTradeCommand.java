f0da1aa50a58e518a506defa07d46b047a7fc516
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-05-13 19:11:01 +0900

    feat: 거래 취소 기능 관련 DTO 필드 수정
    - CancelTradeResult에 eventPublished 필드 추가
    - CancelTradeRequest의 cancelledBy의 타입을 CancellerType enum으로 변경
    - CancelTradeCommand에 tradeId, participantId 필드 추가

--- src/main/java/org/pgsg/trade/application/dto/command/CancelTradeCommand.java
+++ src/main/java/org/pgsg/trade/application/dto/command/CancelTradeCommand.java
@@ -1,11 +1,14 @@
 package org.pgsg.trade.application.dto.command;
 
 import org.pgsg.trade.domain.model.CancelReasonType;
+import org.pgsg.trade.domain.model.CancellerType;
 
 import java.util.UUID;
 
 public record CancelTradeCommand(
-		UUID cancelledBy,
+		UUID tradeId,
+		UUID participantId,
+		CancellerType cancelledBy,
 		CancelReasonType cancelReasonType,
 		String cancelReasonDetail
 ) {
